36f7900e49824cfc964ea4204079e6ece7a6223b
beeena <seemamir13@gmail.com>
2018-11-26 00:20:16 +0500

    changes

--- app/containers/Home/api.js
+++ app/containers/Home/api.js
@@ -3,4 +3,5 @@ import axios from '../../utils/http';
 
 export const fetchPosts = payload =>
   axios.get(`api/post/?category=${get(payload, 'category', '')}`);
+
 export const fetchUser = email => axios.get(`api/user/?email=${email}`);
